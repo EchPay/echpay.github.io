88e360a2e08efe5b03a020487f239dc5837cf1b0
Phuoc <phuocnguyen286@gmail.com>
2020-07-15 09:47:53 +0700

    [ADD] Add page Login with FB

---
+++ precache-manifest.67d5b69575a6340b89e2f97234b1dd26.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d3d4545990763b5b6c662dcaf282ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cc86f11e1f29a70b628",
+    "url": "/static/css/2.85b71462.chunk.css"
+  },
+  {
+    "revision": "314b9128bdf5ad039605",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1cc86f11e1f29a70b628",
+    "url": "/static/js/2.d668c4ba.chunk.js"
+  },
+  {
+    "revision": "549770075ee0851cb4556aa94e45e4a5",
+    "url": "/static/js/2.d668c4ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "314b9128bdf5ad039605",
+    "url": "/static/js/main.2c166566.chunk.js"
+  },
+  {
+    "revision": "35ba65adb81d0c15386b",
+    "url": "/static/js/runtime-main.b77b2b59.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
